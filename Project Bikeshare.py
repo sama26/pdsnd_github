2f5e2be1eb5fa3a664f700ec29cfae089d858a3e
sama26 <58335184+sama26@users.noreply.github.com>
2021-02-10 12:28:08 +0000

    refactor: CITY_DATA variable had been changed to lower case in previous commit without refactoring. This has now been refactored

--- Project Bikeshare.py
+++ Project Bikeshare.py
@@ -71,7 +71,7 @@ def load_data(city, month, day):
     """
 
     # load data file into a dataframe
-    df = pd.read_csv(CITY_DATA[city])
+    df = pd.read_csv(city_data[city])
 
     # convert the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
